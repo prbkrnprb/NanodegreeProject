27b7c4ea50dfc365585d853e6591d35a26d98987
prbkrnprb <mailme@prabakaran.in>
2015-10-04 18:58:03 +0530

    Portfolio created

--- app/src/main/java/in/prabakaran/nanodegreeproject/PortfolioFragment.java
+++ app/src/main/java/in/prabakaran/nanodegreeproject/PortfolioFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class PortfolioFragment extends Fragment {
+public class PortfolioFragment extends Fragment implements Button.OnClickListener{
 
     public PortfolioFragment() {
     }
@@ -17,6 +19,47 @@ public class PortfolioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_portfolio, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_portfolio, container, false);
+        Button spotifyStreamerBtn = (Button) rootView.findViewById(R.id.spotifyStreamerBtn);
+        Button scoresBtn = (Button) rootView.findViewById(R.id.scoresBtn);
+        Button libraryBtn = (Button) rootView.findViewById(R.id.libraryBtn);
+        Button buildItBiggerBtn = (Button) rootView.findViewById(R.id.buildItBiggerBtn);
+        Button xyzReaderBtn = (Button) rootView.findViewById(R.id.xyzReaderBtn);
+        Button capstoneBtn = (Button) rootView.findViewById(R.id.capstoneBtn);
+
+        spotifyStreamerBtn.setOnClickListener(this);
+        scoresBtn.setOnClickListener(this);
+        libraryBtn.setOnClickListener(this);
+        buildItBiggerBtn.setOnClickListener(this);
+        xyzReaderBtn.setOnClickListener(this);
+        capstoneBtn.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String toastText = "";
+        switch (v.getId()){
+            case R.id.spotifyStreamerBtn:
+                toastText = getString(R.string.spotify_streamer);
+                break;
+            case R.id.scoresBtn:
+                toastText = getString(R.string.scores);
+                break;
+            case R.id.libraryBtn:
+                toastText = getString(R.string.library);
+                break;
+            case R.id.buildItBiggerBtn:
+                toastText = getString(R.string.build_it_bigger);
+                break;
+            case R.id.xyzReaderBtn:
+                toastText = getString(R.string.xyz_reader);
+                break;
+            case R.id.capstoneBtn:
+                toastText = getString(R.string.capstone);
+        }
+        toastText = getString(R.string.toast_prefix_text) + toastText;
+        Toast.makeText(v.getContext(), toastText, Toast.LENGTH_LONG).show();
     }
 }
